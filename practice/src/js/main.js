403d929a0780bcff4827a7f1bb85e4c14ef97971
George <klimovgg@gmail.com>
2019-12-12 22:38:35 +0100

    refact: modal window in main js

--- practice/src/js/main.js
+++ practice/src/js/main.js
@@ -3,40 +3,59 @@ document.addEventListener("DOMContentLoaded", function(event) {
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close')
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
+  // const switchModal = () => {
+  //   modal.classList.toggle('modal--visible');
+  // }
+  // modalBtn.forEach(element => {
+  //   element.addEventListener('click', switchModal);
+  // });
 
   // modal.addEventListener('click', switchModal);
   
-  closeBtn.addEventListener('click', switchModal);
+  // closeBtn.addEventListener('click', switchModal);
   document.onkeydown = function(evt) {
     
     if (evt.keyCode == 27) {
       modal.classList.remove('modal--visible');
     }
   };
-  const modalVsbl = document.getElementsByClassName('modal')[0];
-
-// Menu Close
-function modalClose() {
-  modalVsbl.classList.toggle('modal--visible');
-}
-
-// Menu Close Click Outside
-function modalCloseClickOutside(e) {
-    if(!e.target.matches('.modal, .modal *')){
-        modalClose();
-    }
-}
-
-document.addEventListener('click', modalCloseClickOutside);
-document.addEventListener('touchstart', modalCloseClickOutside);
-
 });
+
+
+toggleModal = () => {
+  const modal = document.querySelector('.modal'),
+        modalBtn = document.querySelector('.button__modal'),
+        modalClose = document.querySelector('.modal__close');
+
+        modalBtn.addEventListener('click', (event) => {
+          let target = event.target;
+          if (target.classList.contains('button__modal')) {
+            showModal();
+          }
+        });
+        modalClose.addEventListener('click', (event) => {
+          let target = event.target;
+          if (target.classList.contains('modal__close')) {
+            hideModal();
+          }
+        });
+        modal.addEventListener('click', (event) => {
+          let target = event.target;
+          if (target.classList.contains('modal') && !target.classList.contains('modal__dialog')) {
+            hideModal();
+          }
+        });
+        showModal = () => {
+          modal.classList.add('modal--visible');
+        };
+        hideModal = () => {
+          modal.classList.remove('modal--visible');
+        };
+};
+toggleModal();
+
+
+
 
 
 
